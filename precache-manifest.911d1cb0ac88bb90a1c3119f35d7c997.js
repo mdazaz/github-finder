412e1a1c1d8c2a6d945faae325a1f8eac19d37de
mdazaz <mdazaz2010@gmail.com>
2023-03-09 17:42:47 +0530

    Updates

---
+++ precache-manifest.911d1cb0ac88bb90a1c3119f35d7c997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "07d50479da801b07816e",
+    "url": "/github-finder/static/js/main.07d50479.chunk.js"
+  },
+  {
+    "revision": "de777e5fde807a0452ce",
+    "url": "/github-finder/static/js/1.de777e5f.chunk.js"
+  },
+  {
+    "revision": "df2c2002dc5fc91b744c",
+    "url": "/github-finder/static/css/2.9cbf88d0.chunk.css"
+  },
+  {
+    "revision": "df2c2002dc5fc91b744c",
+    "url": "/github-finder/static/js/2.df2c2002.chunk.js"
+  },
+  {
+    "revision": "3387b6f86fe04d2c726d",
+    "url": "/github-finder/static/js/runtime~main.3387b6f8.js"
+  },
+  {
+    "revision": "b71b7d6046bdfbee99ed17e1c3adecdf",
+    "url": "/github-finder/static/media/spinner.b71b7d60.gif"
+  },
+  {
+    "revision": "ebb215f875f08e9ab790e09a8374b48f",
+    "url": "/github-finder/index.html"
+  }
+];
